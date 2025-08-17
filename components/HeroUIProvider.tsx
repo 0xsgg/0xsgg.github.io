05c79d890faf2abbed7c7c4788fb93226c9aa79e
sggmico <sggmico@gmail.com>
2025-08-17 10:55:10 +0800

    feat: change domain

--- components/HeroUIProvider.tsx
+++ components/HeroUIProvider.tsx
@@ -1,8 +1,9 @@
 'use client';
 import { ReactNode } from 'react';
-import {HeroUIProvider as NextHeroUIProvider} from '@heroui/react';
+import { HeroUIProvider as NextHeroUIProvider } from '@heroui/react';
 
-export function HeroUIProvider({children}: { children: ReactNode }) {
+export function HeroUIProvider({ children }: { children: ReactNode }) {
+
   return (
     <NextHeroUIProvider>
       {children}
