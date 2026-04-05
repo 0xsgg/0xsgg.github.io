a20b18b2c1c13965ad997125555cf7a08b8bbded
0xsgg <sggmico@gmail.com>
2026-04-05 08:00:02 +0800

    feat(config): 添加 Cloudflare/OpenNext 配置

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+// 迁移到 Cloudflare 部署的关键配置
+  output: 'standalone',  // 独立产物/应用
   /* config options here */
-  output: "export",
+  // output: "export",
   // 配置基础路径，这里使用仓库名称作为基础路径
   //basePath: "/",
 
