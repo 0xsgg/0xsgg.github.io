91f78c6cd5cbe6b5c4b7743a19d37a7e0c356ccd
0xsgg <sggmico@gmail.com>
2026-08-03 11:38:57 +0800

    refactor(home): 将简历入口迁移至独立子域名

---
+++ config/data/home.ts
@@ -0,0 +1,47 @@
+export const basics = {
+  email: "sggmico@gmail.com",
+  profiles: [
+    {
+      network: "GitHub",
+      username: "sggmico",
+      url: "https://github.com/sggmico",
+    },
+    {
+      network: "LinkedIn",
+      username: "sggmico",
+      url: "https://www.linkedin.com/in/sggmico",
+    },
+    {
+      network: "Twitter",
+      username: "sggmico",
+      url: "https://twitter.com/sggmico",
+    },
+    {
+      network: "Youtube",
+      username: "sggmico",
+      url: "https://youtube.com/@sggmico",
+    },
+    {
+      network: "Bilibili",
+      username: "sggmico",
+      url: "https://space.bilibili.com/500799076",
+    },
+    {
+      network: "Instagram",
+      username: "sggmico",
+      url: "https://instagram.com/sggmico",
+    },
+    {
+      network: "Portfolio",
+      username: "sggmico",
+      url: "https://sggmico.github.io",
+    },
+  ],
+};
+
+export const navList = [
+  { blank: false, name: "Blog", path: "/blog" },
+  { blank: false, name: "Projects", path: "/projects" },
+  { blank: true, name: "Demos", path: "/demos" },
+  { blank: true, name: "Sponsors", path: "/sponsors" },
+];
